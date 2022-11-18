74e6f112fbfafc9d60a4d4e3dba33591e6fd8233
Felipe Gama Danta <epilefgama2@gmail.com>
2022-11-18 16:11:27 -0300

    fix: requisito 1, fix lint and cpmplex

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,15 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    if target_time == None:
-        return None
-
     students_on_target_time = 0
-    try:
-        for sign_in, sign_out in permanence_period:
-            if type(sign_in) != int or type(sign_out) != int:
-                return None
-            if sign_in <= target_time <= sign_out:
-                students_on_target_time += 1
-    except TypeError:
-        return None
 
-    return students_on_target_time            
+    for sign_in, sign_out in permanence_period:
+        if (
+            type(sign_in) is not int
+            or type(sign_out) is not int
+            or type(target_time) is not int
+        ):
+            return None
+        if sign_in <= target_time <= sign_out:
+            students_on_target_time += 1
+
+    return students_on_target_time
