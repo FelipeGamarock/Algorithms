4a3a1b795d64e243baa2f6e0b7ff4df073d0e4db
Felipe Gama Danta <epilefgama2@gmail.com>
2022-11-18 15:30:18 -0300

    feat: req 1, study_chedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    # start project
-    raise NotImplementedError
+    if target_time == None:
+        return None
+
+    students_on_target_time = 0
+    try:
+        for sign_in, sign_out in permanence_period:
+            if type(sign_in) != int or type(sign_out) != int:
+                return None
+            if sign_in <= target_time <= sign_out:
+                students_on_target_time += 1
+    except TypeError:
+        return None
+
+    return students_on_target_time            
