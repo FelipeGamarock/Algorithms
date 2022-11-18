779b35559c7ae596cd309fafe59d1486e203af90
Felipe Gama Danta <epilefgama2@gmail.com>
2022-11-18 17:11:55 -0300

    feat: requisito 2, test encrypt

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError) as err:
+        encrypt_message(1, 3)
+    assert "tipo inválido para message" in str(err.value)
+    assert err.type == TypeError
+
+    with pytest.raises(TypeError) as err:
+        encrypt_message('AABBCC', '3')
+    assert "tipo inválido para key" in str(err.value)
+    assert err.type == TypeError
+
+    assert encrypt_message('AABBCC', 3) == "BAA_CCB"
+    assert encrypt_message('ABBCCA', 4) == "AC_CBBA"
+    assert encrypt_message('AABBCC', -1) == "CCBBAA"
