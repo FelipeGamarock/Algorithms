d5a88348f1b734d227bebf5071c6ca459311f33d
Felipe Gama Danta <epilefgama2@gmail.com>
2022-11-22 15:42:10 -0300

    fix: lint req 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,7 +2,7 @@ def find_duplicate(nums):
     quick_sort(nums, 0, len(nums) - 1)
     old_num = 0
     for num in nums:
-        if type(num) is not int or num <= 0  :
+        if type(num) is not int or num <= 0:
             return False
         if num == old_num:
             return num
@@ -13,9 +13,9 @@ def find_duplicate(nums):
 # Algoritmo de Quick Sort retirado do Course
 def quick_sort(numbers, start, end):
     if start < end:
-        p = partition(numbers, start, end) 
-        quick_sort(numbers, start, p - 1) 
-        quick_sort(numbers, p + 1, end) 
+        p = partition(numbers, start, end)
+        quick_sort(numbers, start, p - 1)
+        quick_sort(numbers, p + 1, end)
 
 
 def partition(numbers, start, end):
@@ -25,8 +25,11 @@ def partition(numbers, start, end):
     for index in range(start, end):
 
         if numbers[index] <= pivot:
-          delimiter = delimiter + 1
-          numbers[index], numbers[delimiter] = numbers[delimiter], numbers[index]
+            delimiter = delimiter + 1
+            numbers[index], numbers[delimiter] = (
+                numbers[delimiter],
+                numbers[index],
+            )
 
     numbers[delimiter + 1], numbers[end] = numbers[end], numbers[delimiter + 1]
 
